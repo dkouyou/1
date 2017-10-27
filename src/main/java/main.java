7cb8cd271d2c1093620a8579068f59c41ea2e36b
Charles Pauze <kouyou.junk@gmail.com>
2017-10-27 12:58:02 -0400

    tout

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(compter(5,4));
+
+	}
+	
+	public static int compter(int l , int i) {
+		
+		return i+l;
+	}
+
+}
